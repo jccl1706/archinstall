1f39213b8cd55ce0691c1003e3098fdeade0038e
jc <jccl1706@gmai.com>
2024-01-04 15:52:06 -0500

    new changes

--- .config/polybar/scripts/launch_polybar.sh
+++ .config/polybar/scripts/launch_polybar.sh
@@ -6,6 +6,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar -c ~/.config/polybar/config.ini left &
 polybar -c ~/.config/polybar/config.ini xwindow &
 polybar -c ~/.config/polybar/config.ini center &
-polybar -c ~/.config/polybar/config.ini tray &
+#polybar -c ~/.config/polybar/config.ini tray &
 polybar -c ~/.config/polybar/config.ini right &
 polybar -c ~/.config/polybar/config.ini poweroff &
