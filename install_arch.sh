3b48fe9ad2b136014c24921e002e3d2bb2e2bc29
jc <jccl1706@gmail.com>
2024-01-19 12:30:02 -0500

    new changes

--- install_arch.sh
+++ install_arch.sh
@@ -45,7 +45,7 @@ guipacs=(
         # plasma
         # plasma-wayland-session
         # sddm
-        # kitty
+        kitty
         # firefox
         # alacritty
         # nm-connection-editor
@@ -65,19 +65,23 @@ sgdisk \
 sleep 2
 partprobe -s "$target"
 sleep 2
+
 echo "Making File Systems..."
 # Create file systems
 mkfs.vfat -F32 -n EFISYSTEM /dev/disk/by-partlabel/EFISYSTEM
 mkfs.ext4 -L linux "$rootpart"
+mkswap "$swappart"
+
 # mount the root, and create + mount the EFI directory
 echo "Mounting File Systems..."
 mount "$rootpart" "$rootmnt"
 mkdir "$rootmnt"/boot/efi -p
 mount -t vfat /dev/disk/by-partlabel/EFISYSTEM "$rootmnt"/boot/efi
 sleep 2
+
 # mount & activating swap partition
-echo "Activating & Mounting SWAP partition"
-mkswap "$swappart"
+echo "Activating SWAP partition"
+
 swapon "$swappart"
 sleep 2
 
@@ -92,8 +96,10 @@ sleep 2
 
 echo "Setting up environment..."
 #set up locale/env
+
 #add our locale to locale.gen
 sed -i -e "/^#"$locale"/s/^#//" "$rootmnt"/etc/locale.gen
+
 #remove any existing config files that may have been pacstrapped, systemd-firstboot will then regenerate them
 rm "$rootmnt"/etc/{machine-id,localtime,hostname,shadow,locale.conf} ||
 systemd-firstboot --root "$rootmnt" \
@@ -101,56 +107,30 @@ systemd-firstboot --root "$rootmnt" \
 	--locale-messages="$locale" --timezone="$timezone" \
 	--hostname="$hostname" --setup-machine-id \
 	--welcome=false
+
+#add terminus font to console
 sed -i '/^KEYMAP/a\FONT=ter-124b' "$rootmnt"/etc/vconsole.conf
 arch-chroot "$rootmnt" locale-gen
 echo "Configuring for first boot..."
+
 #add the local user
 arch-chroot "$rootmnt" useradd -G wheel -m -p "$user_password" "$username"
 #uncomment the wheel group in the sudoers file
 sed -i -e '/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^# //' "$rootmnt"/etc/sudoers
-# #create a basic kernel cmdline, we're using DPS so we don't need to have anything here really, but if the file doesn't exist, mkinitcpio will complain
-# echo "quiet rw" > "$rootmnt"/etc/kernel/cmdline
-# #change the HOOKS in mkinitcpio.conf to use systemd hooks
-# sed -i \
-#     -e 's/base udev/base systemd/g' \
-#     -e 's/keymap consolefont/sd-vconsole sd-encrypt/g' \
-#     "$rootmnt"/etc/mkinitcpio.conf
-# #change the preset file to generate a Unified Kernel Image instead of an initram disk + kernel
-# sed -i \
-#     -e '/^#ALL_config/s/^#//' \
-#     -e '/^#default_uki/s/^#//' \
-#     -e '/^#default_options/s/^#//' \
-#     -e 's/default_image=/#default_image=/g' \
-#     -e "s/PRESETS=('default' 'fallback')/PRESETS=('default')/g" \
-#     "$rootmnt"/etc/mkinitcpio.d/linux.preset
-#
-# #read the UKI setting and create the folder structure otherwise mkinitcpio will crash
-# declare $(grep default_uki "$rootmnt"/etc/mkinitcpio.d/linux.preset)
-# arch-chroot "$rootmnt" mkdir -p "$(dirname "${default_uki//\"}")"
-#
-# #install the gui packages
-# echo "Installing GUI..."
-# arch-chroot "$rootmnt" pacman -Sy "${guipacs[@]}" --noconfirm --quiet
-#
-#
-# #enable the services we will need on start up
-# echo "Enabling services..."
-# systemctl --root "$rootmnt" enable systemd-resolved systemd-timesyncd NetworkManager
-# #mask systemd-networkd as we will use NetworkManager instead
-# systemctl --root "$rootmnt" mask systemd-networkd
-# #regenerate the ramdisk, this will create our UKI
-# echo "Generating UKI and installing Boot Loader..."
-# arch-chroot "$rootmnt" mkinitcpio -p linux
-# echo "Setting up Secure Boot..."
-# if [[ "$(efivar -d --name 8be4df61-93ca-11d2-aa0d-00e098032b8c-SetupMode)" -eq 1 ]]; then
-# arch-chroot "$rootmnt" sbctl create-keys
-# arch-chroot "$rootmnt" sbctl enroll-keys -m
-# arch-chroot "$rootmnt" sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
-# arch-chroot "$rootmnt" sbctl sign -s "${default_uki//\"}"
-# else
-# echo "Not in Secure Boot setup mode. Skipping..."
-# fi
-# #install the systemd-boot bootloader
+
+#install the gui packages
+echo "Installing GUI..."
+arch-chroot "$rootmnt" pacman -Sy "${guipacs[@]}" --noconfirm --quiet
+
+#enable the services we will need on start up
+echo "Enabling services..."
+systemctl --root "$rootmnt" enable NetworkManager
+
+#generating initial ramdisk
+echo "Generating  initial ramdisk..."
+arch-chroot "$rootmnt" mkinitcpio -p linux
+
+#install the GRUB bootloader
 # arch-chroot "$rootmnt" bootctl install --esp-path=/efi
 # #lock the root account
 # arch-chroot "$rootmnt" usermod -L root
